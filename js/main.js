70d14cec577b9e77df140321e75c63ddb65f2cea
YueOX <yuetchan@umich.edu>
2017-09-11 16:06:08 -0400

    daily updates

--- js/main.js
+++ js/main.js
@@ -28,9 +28,11 @@
       $(this).addClass('active');
 
       var target = this.hash;
-      $target = $(target);
+      // console.log(this.hash);
+      // console.log($(target));
+      // $target = $(target);
       $('html, body').stop().animate({
-        'scrollTop': $target.offset().top+2
+        'scrollTop': $(target).offset().top+2
       }, 500, 'swing', function () {
         window.location.hash = target;
         $(document).on("scroll", onScroll);
