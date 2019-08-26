809389af2d5018f12f380226e31085fd8bdec2c5
YueOX <yuetchan@umich.edu>
2019-08-26 12:00:43 -0400

    homepage reorg

--- js/main.js
+++ js/main.js
@@ -58,36 +58,36 @@
     closebtn = document.getElementById( 'close-button' ),
     isOpen = false;
 
-    function inits() {
-      initEvents();
-    }
-
-    function initEvents() {
-      openbtn.addEventListener( 'click', toggleMenu );
-      if( closebtn ) {
-        closebtn.addEventListener( 'click', toggleMenu );
-      }
+    // function inits() {
+    //   initEvents();
+    // }
+    //
+    // function initEvents() {
+    //   openbtn.addEventListener( 'click', toggleMenu );
+    //   if( closebtn ) {
+    //     closebtn.addEventListener( 'click', toggleMenu );
+    //   }
 
       // close the menu element if the target it´s not the menu element or one of its descendants..
-      content.addEventListener( 'click', function(ev) {
-        var target = ev.target;
-        if( isOpen && target !== openbtn ) {
-          toggleMenu();
-        }
-      } );
-    }
-
-    function toggleMenu() {
-      if( isOpen ) {
-        classie.remove( bodyEl, 'show-menu' );
-      }
-      else {
-        classie.add( bodyEl, 'show-menu' );
-      }
-      isOpen = !isOpen;
-    }
-
-    inits();
+    //   content.addEventListener( 'click', function(ev) {
+    //     var target = ev.target;
+    //     if( isOpen && target !== openbtn ) {
+    //       toggleMenu();
+    //     }
+    //   } );
+    // }
+    //
+    // function toggleMenu() {
+    //   if( isOpen ) {
+    //     classie.remove( bodyEl, 'show-menu' );
+    //   }
+    //   else {
+    //     classie.add( bodyEl, 'show-menu' );
+    //   }
+    //   isOpen = !isOpen;
+    // }
+    //
+    // inits();
 
 
     //typed js
